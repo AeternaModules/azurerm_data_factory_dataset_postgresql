97b449f6452d2ffbb6b0dd64f1f190f999561edd
Daniel Allen <dan@aeternagroup.net>
2026-07-12 13:19:18 +0100

    Update to schema version v4.80.0

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ resource "azurerm_data_factory_dataset_postgresql" "data_factory_dataset_postgre
   table_name            = each.value.table_name
 
   dynamic "schema_column" {
-    for_each = each.value.schema_column != null ? [each.value.schema_column] : []
+    for_each = each.value.schema_column != null ? each.value.schema_column : []
     content {
       description = schema_column.value.description
       name        = schema_column.value.name
